5fd60124f70fb57f28966ab2719644f9de75be31
Matheus Franco <cajondefox@gmail.com>
2024-09-29 22:26:50 -0300

    feat(DMR01): add pages routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,49 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { 
+  PreloadAllModules,
+  RouterModule,
+  Routes,
+} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'duel',
-    loadChildren: () => import('./duel/duel.module').then( m => m.DuelPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+  },
+  {
+    path: 'duel',
+    loadChildren: () => import('./duel/duel.module').then( m => m.DuelPageModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+  },
+  {
+    path: 'duelist',
+    loadChildren: () => import('./duelist/duelist.module').then( m => m.DuelistPageModule),
+  },
+  {
+    path: 'card',
+    loadChildren: () => import('./card/card.module').then( m => m.CardPageModule),
+  },  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
+  },
+
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(
+      routes,
+      { preloadingStrategy: PreloadAllModules },
+    ),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
